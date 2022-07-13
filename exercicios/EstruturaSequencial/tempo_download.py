da6eb5589a42f26fe26fdd0528e117e07f73c3cc
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2022-07-13 19:19:34 -0300

    Fazendo os exercícios Estrutura de Decisão

--- exercicios/EstruturaSequencial/tempo_download.py
+++ exercicios/EstruturaSequencial/tempo_download.py
@@ -4,6 +4,6 @@ Faça um programa que peça o tamanho de um arquivo para descarregar (em MB) e a
 """
 arquivo = float(input('Qual o tamanho do arquivo que quer fazer download: '))
 velocidade = int(input('Qual a velocidade da internet: ')) / 8
-download_minuto = (arquivo / velocidade) / 60
-download_segundo = (arquivo / velocidade) % 60
-print(f'O tempo aproximado para o download é de {download_minuto:.0f} minuto(s) e {download_segundo:.0f} segundo(s)')
+download_minuto = int((arquivo / velocidade) / 60)
+download_segundo = int((arquivo / velocidade) % 60)
+print(f'O tempo aproximado para o download é de {download_minuto} minuto(s) e {download_segundo} segundo(s)')
