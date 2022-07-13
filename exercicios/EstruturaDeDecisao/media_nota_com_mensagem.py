89938bd0686f40473d9961969c55fcf0c5498571
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2022-07-12 22:37:40 -0300

    Fazendo os exercícios Estrutura de Decisão

---
+++ exercicios/EstruturaDeDecisao/media_nota_com_mensagem.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+-A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+-A mensagem "Reprovado", se a média for menor do que sete;
+-A mensagem "Aprovado com Distinção", se a média for igual a dez.
+"""
+try:
+    nota1 = float(input('Digite a primeira nota: '))
+    nota2 = float(input('Digite a segunda nota: '))
+except ValueError:
+    print('Você não digitou um número em algum momento.')
+else:
+    media = (nota1 + nota2) / 2
+    print(f'sua média foi {media}.')
+    if media >= 10:
+        print('Aprovado com Distinção')
+    elif media >= 7:
+        print('Aprovado')
+    else:
+        print('Reprovado')
