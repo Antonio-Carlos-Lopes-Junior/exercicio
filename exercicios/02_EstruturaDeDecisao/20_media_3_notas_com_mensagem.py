8aa2cd395487cc3d1c4e46b2faf1e518d2e7139a
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2022-07-21 18:12:29 -0300

    Feito o 20.º exercício de Estrutura de decisão

--- exercicios/02_EstruturaDeDecisao/20_media_3_notas_com_mensagem.py
+++ exercicios/02_EstruturaDeDecisao/20_media_3_notas_com_mensagem.py
@@ -12,11 +12,14 @@ try:
 except ValueError:
     print('Você não digitou um número em algum momento.')
 else:
-    media = (nota1 + nota2 + nota3) / 3
-    print(f'sua média foi {media}.')
-    if media >= 10:
-        print('Aprovado com Distinção')
-    elif media >= 7:
-        print('Aprovado')
+    if nota1 < 0 or nota2 < 0 or nota3 < 0:
+        print('Você digitou nota negativa em algum momento. ')
     else:
-        print('Reprovado')
+        media = (nota1 + nota2 + nota3) / 3
+        print(f'sua média foi {media:.1f}.')
+        if media >= 10:
+            print('Aprovado com Distinção')
+        elif media >= 7:
+            print('Aprovado')
+        else:
+            print('Reprovado')
