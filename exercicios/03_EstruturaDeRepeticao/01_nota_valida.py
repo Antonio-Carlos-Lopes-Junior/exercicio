ff36ee852b03f87a0c1ab716adf44af7eadab629
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2022-08-08 20:45:49 -0300

    Feito o 1.º exercício de Estrutura de repetição

--- exercicios/03_EstruturaDeRepeticao/01_nota_valida.py
+++ exercicios/03_EstruturaDeRepeticao/01_nota_valida.py
@@ -2,15 +2,11 @@
 Faça um programa que peça uma nota, entre zero e dez.
 Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 """
-try:
-    nota = float(input('Digite uma nota entre 0 e 10: '))
-except ValueError:
-    print('Não foi digitado um número.')
-else:
-    while nota < 0 or nota > 10:
-        try:
-            nota = float(input('Digite uma nota entre 0 e 10: '))
-        except ValueError:
-            print('Não foi digitado um número.')
+nota = -1
+while nota < 0 or nota > 10:
+    try:
+        nota = float(input('Digite uma nota entre 0 e 10: '))
+    except ValueError:
+        print('Não foi digitado um número.')
 
-    print(f'A nota digitada foi: {nota:.1f}')
+print(f'A nota digitada foi: {nota:.1f}')
